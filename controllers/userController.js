42f5df23b04978bfabab571f585509f0ada39016
Dwivedi <navneetdwivedi@live.com>
2017-10-28 23:50:07 +0530

    controller is added in users

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+var mongo_data  = require('../models/mongo_utils');
+
+var users ={};
+
+users.login = function (req, res, next) {
+    var username = req['query']['username'];
+    var password = req['query']['password'];
+    var data = mongo_data.getUser(username,password);
+    data.then(function(result) {
+        console.log(result)
+        res.send('respond with login '+result[0]['username']+' resource ');
+    })
+};
+
+users.update = function(req, res, next) {
+    var username = req['query']['username'];
+    var password = req['query']['password'];
+    var name = req['query']['name'];
+    var email = req['query']['email'];
+    var dob = req['query']['dob'];
+    var status = req['query']['status'];
+    var respond  = mongo_data.updateUser(username,password,name,email,dob,status);
+    res.send('respond with update resource');
+};
+
+users.registration = function(req, res, next) {
+    var username = req['query']['username'];
+    var password = req['query']['password'];
+    var name = req['query']['name'];
+    var email = req['query']['email'];
+    var dob = req['query']['dob'];
+    var status = req['query']['status'];
+    var respond  = mongo_data.saveUser(username,password,name,email,dob,status);
+    res.send('saved data for '+req['query']['username']+' resource ');
+};
+
+module.exports = users;
--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var mongo_data  = require('../models/mongo_utils');
+var users = require('../controllers/userController');
 
-router.get('/registration', function(req, res, next) {
-    var username = req['query']['username'];
-    var password = req['query']['password'];
-    var name = req['query']['name'];
-    var email = req['query']['email'];
-    var dob = req['query']['dob'];
-    var status = req['query']['status'];
-    var respond  = mongo_data.saveUser(username,password,name,email,dob,status);
-    res.send('saved data for '+req['query']['username']+' resource ');
-});
+router.get('/registration', users.registration);
+
+function mdl(req, res, next){
+    next();
+}
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-    var username = req['query']['username'];
-    var password = req['query']['password'];
-    var data = mongo_data.getUser(username,password);
-    data.then(function(result) {
-        console.log(result)
-        res.send('respond with login '+result[0]['username']+' resource ');
-    })
-});
+router.get('/login',mdl,users.login);
 
-
-router.get('/update', function(req, res, next) {
-    var username = req['query']['username'];
-    var password = req['query']['password'];
-    var name = req['query']['name'];
-    var email = req['query']['email'];
-    var dob = req['query']['dob'];
-    var status = req['query']['status'];
-    var respond  = mongo_data.updateUser(username,password,name,email,dob,status);
-    res.send('respond with update resource');
-});
+/* update users */
+router.get('/update', users.update);
 
 module.exports = router;
